c74820c540a3aae7c7496925e831d9bfb571d964
Dimitris Papadimitriou <jim_pap@live.com>
2024-10-13 20:34:32 +0300

    corrected rule typos

--- src/gameoflife.c
+++ src/gameoflife.c
@@ -16,23 +16,99 @@ void wait_for(unsigned int seconds);
 int main(void){
     
     char **world;
-    world = init_world();
-    world[7][3]=1;
-    world[7][4]=1;
-    world[7][5]=1;
+    char **neighbor_count;
 
-    print_world(world);
+    world = init_world();
+    neighbor_count = init_world();
+
+    //crawler
+    world[2][1]=1;
+    world[2][3]=1;
+    world[3][2]=1;
+    world[3][3]=1;
+    world[4][2]=1;
+
+    // world[2][1]=1;
+    // world[2][2]=1;
+    // world[2][3]=1;
     
+    print_world(world);
+    putc('\n',stdout);
+    // printf("(1+10)%%10=%d\n",(1+10)%10);
+    // printf("(-1+10)%%10=%d\n",(-1+10)%10);
+    while(1){
+        //game loop
+
+        //loop through internal cells
+        for(int x=0;x<WORLD_WIDTH;x++)
+        {
+            for (int y=0;y<WORLD_HEIGHT;y++)
+            {
+                //get neighbor count
+                switch (world[x][y])
+                {
+                case 1://alive
+                    //vote to neighbors
+                    int x_minus, x_pluss, y_minus, y_pluss;
+                    x_minus = (x-1+WORLD_WIDTH)%WORLD_WIDTH;
+                    x_pluss = (x+1)%WORLD_WIDTH;
+                    y_minus = (y-1+WORLD_HEIGHT)%WORLD_HEIGHT;
+                    y_pluss = (y+1)%WORLD_HEIGHT;
+
+
+                    neighbor_count[x_minus][y_minus]+=1;
+                    neighbor_count[x][y_minus]+=1;
+                    neighbor_count[x_pluss][y_minus]+=1;
+                    neighbor_count[x_minus][y]+=1;
+                    neighbor_count[x_pluss][y]+=1;
+                    neighbor_count[x_minus][y_pluss]+=1;
+                    neighbor_count[x][y_pluss]+=1;
+                    neighbor_count[x_pluss][y_pluss]+=1;
+                    break;
+                
+                default:
+                    break;
+                }
+            }
+        }
+        
+        //decide for each cell
+        for (int y=0;y<WORLD_HEIGHT;y++){
+            for(int x=0;x<WORLD_WIDTH;x++){
+                if(world[x][y]==1){
+                    //alive_cell case
+                    if( !(neighbor_count[x][y]==2 || neighbor_count[x][y]==3) )
+                        world[x][y]=0;
+
+                }else{
+                    //dead cell
+                    if(neighbor_count[x][y] == 3)
+                        world[x][y]=1;
+                }
+            }
+        }
+        //print world
+        print_world(world);
+        //reinitialize neighbor count
+        for (int y=0;y<WORLD_HEIGHT;y++){
+            for(int x=0;x<WORLD_WIDTH;x++){
+                neighbor_count[x][y]=0;
+            }
+        }
+        wait_for(1);
+        
+        puts("\n");
+    }
     return 0;
 }
 
 void print_world(char **world)
 {
     int x,y;
-
+    //system("clear");
     for (y=0;y<WORLD_HEIGHT;y++){
         for(x=0;x<WORLD_WIDTH;x++){
-            (world[x][y])?putchar(CELL_ALIVE):putchar(CELL_DEAD);
+            (world[x][y]==1)?putchar(CELL_ALIVE):putchar(CELL_DEAD);
         }
         putchar('\n');
     }
@@ -56,6 +132,6 @@ char **init_world(void){
 }
 void wait_for(unsigned int seconds)
 {
-    unsigned int retTime = time(0) + seconds;   // Get finishing time.
+    long int retTime = time(0) + seconds;   // Get finishing time.
     while (time(0) < retTime);               // Loop until it arrives.
 }
